f1b30c6b87403fd86d27c59ad1408e6607f5cc39
PedroGelisson <93836001+PedroGelisson@users.noreply.github.com>
2022-12-12 20:54:48 -0300

    Update listaPessoas.php

--- ListaCadastro/listaPessoas.php
+++ ListaCadastro/listaPessoas.php
@@ -4,16 +4,20 @@
         $sql="SELECT * FROM lista";
         $resultado=$conexao->query($sql);
         
+
         while($lista=$resultado->fetch_array()){
             echo '<div class="card" >
             <img class="card-img-top" src="'.$lista['foto'].'" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">'.$lista['nome'].'</h5>
                 <p class="card-text">'.$lista['email'].'</p>
-                <a href="#" class="card-link" style="text-decoration:none;">Editar</a>
-                <a href="#" class="card-link" style="text-decoration:none; color:#f00;">Excluir</a>
+                <a class="card-link" style="text-decoration:none;" href="editarLista.php?id='.$lista['id'].'" >Editar</a>
+                <a class="card-link" style="text-decoration:none; color: #f00;" href= "excluirLista.php?id='.$lista['id'].'">Excluir</a>
             </div>    
             </div>';
         }
-    ?>
+    
+         ?>
     </div>
+
+    
